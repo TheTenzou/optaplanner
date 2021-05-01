810d2b6d80154d4fdde8d1ab77b36adb691a2cd4
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-05-01 14:18:13 +0300

    Initial commit

---
+++ src/main/java/ru/thetenzou/optaplanertests/OptaPlannerTestsApplication.java
@@ -0,0 +1,13 @@
+package ru.thetenzou.optaplanertests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptaPlannerTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptaPlannerTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/thetenzou/optaplanertests/OptaplanerTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.thetenzou.optaplanertests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptaplanerTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
