fb41d6d7ee3bc8ed8644a538fe98dc8f7dfe29ac
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-05-01 15:03:23 +0300

    Add controller

--- src/main/java/ru/thetenzou/optaplanertests/controller/TimeTableController.java
+++ src/main/java/ru/thetenzou/optaplanertests/controller/TimeTableController.java
@@ -1,12 +1,30 @@
 package ru.thetenzou.optaplanertests.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.thetenzou.optaplanertests.model.TimeTable;
+import ru.thetenzou.optaplanertests.repository.TimeTableRepository;
 import ru.thetenzou.optaplanertests.repository.TimeslotRepository;
 
 @RestController
 @RequestMapping("/timeTable")
 public class TimeTableController {
+
+    @Autowired
+    private TimeTableRepository timeTableRepository;
+
+    @GetMapping
+    public TimeTable getTimeTable() {
+        return timeTableRepository.findById(1L);
+    }
+
+    @PostMapping("/solve")
+    public void solve() {
+        throw new UnsupportedOperationException();
+    }
+
 
 }
